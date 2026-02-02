4c88233cdead3f34326776be8cce73514ac87566
stevicamp <archangel75@abv.bg>
2026-02-03 00:06:20 +0200

    ok

--- static/js/resources/default_scripts.js
+++ static/js/resources/default_scripts.js
@@ -1388,11 +1388,11 @@ async function getItems(itemType, itemsList)  // ItemType = car, caravan, produc
          <div class="item_buttons_wrapper"> 
              <a class="item_share_button" style="background-image: url('static/img/icons/copy.png');" href="javascript:copyToClipboard('${itemLink}');"></a>
              <a class="item_share_button" style="background-image: url('static/img/icons/viber.png');"
-                 href="javascript:void(0)" onclick="window.location.href = 'viber://forward?text=${itemLink}'"></a>
+                 href="javascript:void(0)" onclick="window.location.href='viber://forward?text=${itemLink}'"></a>
              <a class="item_share_button" style="background-image: url('static/img/icons/whatsapp.png');"
-             target="_blank" rel="noopener noreferrer" href="javascript:void(0)" onclick="window.location.href = 'https://api.whatsapp.com/send?text=${itemLink}'"></a>
+             target="_blank" rel="noopener noreferrer" href="javascript:void(0)" onclick="window.location.href='https://api.whatsapp.com/send?text=${itemLink}'"></a>
              <a class="item_share_button" style="background-image: url('static/img/icons/messenger.png');"
-                 href="javascript:void(0)" onclick="window.location.href = 'fb-messenger://share/?link=${itemLink}'"></a>
+                 href="javascript:void(0)" onclick="window.location.href='fb-messenger://share/?link=${itemLink}'"></a>
          </div>
         
          </div>`);
